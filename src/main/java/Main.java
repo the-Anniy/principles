d18a24bbcde56dfb23bc93b788483614398b80ce
Anna Paliy <annpaliy2015@gmail.com>
2024-12-02 14:22:30 +0300

    Code reformated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Post post = new Post();
         post.birthday = new FormDate();
         post.birthday.day = 13;
@@ -7,9 +7,9 @@ public class Main {
         post.birthday.year = 1999;
         post.name = "Иван";
         post.passport = "4444 №44444444";
-        post.patronymic =  "Иванович";
+        post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription =true;
+        post.subscription = true;
     }
 }

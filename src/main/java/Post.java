d39cf57ea84c456418fdac4ceaeaf4fa1d0579de
Anna Paliy <annpaliy2015@gmail.com>
2024-12-02 14:16:10 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday;
+}
